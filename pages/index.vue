ca1f5355cd7d63f322b2d5f7951f7a498d24a7da
Narendra Krishna Ram <nkr@aroliant.com>
2020-03-19 17:48:07 +0530

    remove old content and create new design and content

--- pages/index.vue
+++ pages/index.vue
@@ -1,277 +1,126 @@
 <template>
   <div>
-    <div class="bg-white position-absolute w-100 mt-5" style="z-index:2;">
-      <div class="container p-0">
-        <nav class="navbar navbar-expand navbar-light px-3">
-          <img src="~/assets/initial.png" class="img-fluid Initial" />
-          <div class="nav navbar-nav ml-auto">
-            <a class="Header__link mr-3" target="_blank" href="https://twitter.com/nkr_96">Blog</a>
-            <a
-              class="Header__link"
-              target="_blank"
-              href="https://www.upwork.com/freelancers/~01d7fc59aedbaa665c"
-            >Hire Me</a>
-          </div>
-        </nav>
-      </div>
+    <div class="Header">
+      <nav class="container navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="#">Narendra Ram</a>
+        <button
+          class="navbar-toggler d-lg-none"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Hire Me</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
 
-    <div class="Section bg-white Intro">
+    <section>
       <div class="container">
         <div class="row">
-          <div class="col-12 text-left">
-            <h1 class="text-dark Name">
-              Hi! I’m Narendra.
-              <br />I design stuff.
-            </h1>
-          </div>
-        </div>
-      </div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-    </div>
-
-    <div class="Section bg-white Introduction">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 text-center">
-            <img class="img-fluid Myphoto" src="~/assets/images/naren.png" alt="narendra-ram-image" />
-          </div>
-          <div class="col-lg-6 text-center align-self-center">
-            <p
-              class="mb-0 Introduction__text mx-auto"
-            >I'm a Frontend developer based in India. I'm passionate about web, food and environment. Also, i write stuff such as blog articles, creative writing, terms, etc.</p>
-          </div>
-        </div>
-      </div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-    </div>
-
-    <div class="Section bg-white">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-lg-2">
-            <i class="fas fa-2x fa-laptop-code"></i>
-            <h3>Frontend Developer</h3>
-            <p>I like coding my designs. I implement my designs using latest HTML, CSS, and basic javascript.</p>
-          </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-lg-2">
-            <i class="fas fa-2x fa-magic"></i>
-            <h3>UI/UX Designer</h3>
-            <p>I have been working on my own company as a UI/UX designer for the past 2 years.</p>
-          </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-lg-2">
-            <i class="fas fa-2x fa-pencil-alt"></i>
-            <h3>Writer</h3>
-            <p>I write stuff for blog articles, terms, creative writing, official documents, and web content.</p>
-          </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-lg-2">
-            <i class="fas fa-2x fa-chalkboard-teacher"></i>
-            <h3>Mentor</h3>
-            <p>I teach frontend development in my native language for students in Tamilnadu.</p>
-          </div>
-        </div>
-      </div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-      <div class="dots"></div>
-    </div>
-
-    <div class="Section bg-light">
-      <div class="container">
-        <div class="w-100 text-center">
-          <h2>Some of my work</h2>
-        </div>
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-6 col-lg-4 p-3 p-lg-3">
-            <a
-              class="Project card"
-              data-aos="zoom-in"
-              target="_blank"
-              href="https://gtrendz.online/"
-            >
-              <div class="Project__image">
-                <img src="~/assets/images/gtrendz-online-web.png" class="card-img Project__img" alt />
-              </div>
-              <div class="card-body">
-                <h4>GTrendz Online Web</h4>
-                <div class="Project__description">A multi-vendor e-commerce platform.</div>
-              </div>
-            </a>
-          </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-4 p-3 p-lg-3">
-            <a
-              class="Project card"
-              data-aos="zoom-in"
-              target="_blank"
-              href="https://play.google.com/store/apps/details?id=com.gtrendzfw.app.gtrendz"
-            >
-              <div class="Project__image">
-                <img src="~/assets/images/gtrendz-app.png" class="card-img Project__img" alt />
-              </div>
-              <div class="card-body">
-                <h4>GTrendz Online App</h4>
-                <div class="Project__description">ReactNative mobile application.</div>
-              </div>
-            </a>
-          </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-4 p-3 p-lg-3">
-            <a
-              class="Project card"
-              data-aos="zoom-in"
-              target="_blank"
-              href="https://tools.aroliant.com/"
-            >
-              <div class="Project__image">
-                <img src="~/assets/images/devtools-web.png" class="card-img Project__img" alt />
-              </div>
-              <div class="card-body">
-                <h4>DevTools Web</h4>
-                <div class="Project__description">A set of developer tools web app.</div>
-              </div>
-            </a>
-          </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-4 p-3 p-lg-3">
-            <a
-              class="Project card"
-              data-aos="zoom-in"
-              target="_blank"
-              href="https://nkr96.github.io/htmlwebsite"
-            >
-              <div class="Project__image">
-                <img src="~/assets/images/html-multipage.png" class="card-img Project__img" alt />
-              </div>
-              <div class="card-body">
-                <h4>HTML Website</h4>
-                <div class="Project__description">Multipage static html website.</div>
-              </div>
-            </a>
-          </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-4 p-3 p-lg-3">
-            <a class="Project card" data-aos="zoom-in" target="_blank" href="https://aroliant.com/">
-              <div class="Project__image">
-                <img src="~/assets/images/company-web.png" class="card-img Project__img" alt />
-              </div>
-              <div class="card-body">
-                <h4>Aroliant Website</h4>
-                <div class="Project__description">A company website in Nuxt.js</div>
-              </div>
-            </a>
-          </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-4 p-2 p-lg-3">
-            <a
-              class="Project card"
-              data-aos="zoom-in"
-              target="_blank"
-              href="https://seller.gtrendz.online"
-            >
-              <div class="Project__image">
-                <img src="~/assets/images/gtrendz-seller.png" class="card-img Project__img" alt />
-              </div>
-              <div class="card-body">
-                <h4>Seller Dashboard</h4>
-                <div class="Project__description">E-Commerce vendor dashboard.</div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12 text-right py-2 px-2">
-            <button class="btn btn-lg text-white Button mt-3 mb-4 rounded-pill">View More</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="Section Section--half bg-gradient-primary">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 text-center">
-            <h2 class="text-white">Me on the Internet</h2>
+          <div class="col-lg-6">
+            <h1>Hi, I'm Narendra Ram</h1>
+            <h3>Full Stack Designer</h3>
             <div class="SocialLinks">
-              <a class="SocialLink" target="_blank" href="https://twitter.com/nkr_96">
-                <i class="fab fa-3x fa-twitter"></i>
-              </a>
-              <a class="SocialLink" target="_blank" href="https://medium.com/@nkr96">
-                <i class="fab fa-3x fa-medium"></i>
-              </a>
-              <a class="SocialLink" target="_blank" href="https://dev.to/nkr96">
-                <i class="fab fa-3x fa-dev"></i>
-              </a>
-              <a class="SocialLink" target="_blank" href="https://www.linkedin.com/in/nkr96/">
-                <i class="fab fa-3x fa-linkedin"></i>
-              </a>
-              <a class="SocialLink" target="_blank" href="https://www.behance.net/nkr96">
-                <i class="fab fa-3x fa-behance"></i>
-              </a>
-              <a class="SocialLink" target="_blank" href="https://dribbble.com/nkr96">
-                <i class="fab fa-3x fa-dribbble"></i>
-              </a>
-              <a class="SocialLink" target="_blank" href="https://www.deviantart.com/nkr96">
-                <i class="fab fa-3x fa-deviantart"></i>
-              </a>
+              <div class="SocialLink"></div>
+              <div class="SocialLink"></div>
+              <div class="SocialLink"></div>
+              <div class="SocialLink"></div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <div class="Section bg-white">
+    </section>
+    <hr class="container" />
+    <section>
       <div class="container">
+        <h3>Portfolio</h3>
+        <h4 class="FontRegular">Here are some of my works</h4>
         <div class="row">
-          <div class="w-100 text-center">
-            <h1 class="mb-0 Name">
-              Let's C
-              <i class="fab fa-telegram"></i>nnect
-            </h1>
+          <div class="col-lg-6">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">GTrendz Online</h3>
+              <h5>A Multivendor E-Commerce Platform</h5>
+              <p>I worked on the frontend requirements for the website, react-native application, and the angular dashboards. Some of my tasks are such as creating pages, multiple layouts, responsiveness, etc.</p>
+              <a href="https://gtrendz.online" rel="no-follow" class="ProjectLink">website</a>
+            </div>
           </div>
-          <div class="col-12 text-center">
-            <p class="lead mt-2">Feel free to chat about design, food and Netflix.</p>
-            <p class="lead">
-              <a
-                class="btn btn-light btn-lg text-white Button rounded-pill"
-                target="_blank"
-                href="https://t.me/nkr_96"
-                role="button"
-              >Say &#128075; Hello!</a>
-            </p>
+          <div class="col-lg-6">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">Blaz App</h3>
+              <h5>Deployments & Scaling</h5>
+              <p>I have been working on building the frontend part for the blaz app and the landing pages for the website. It’s a work in progress.</p>
+              <a href="https://blaz.app/" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">Instant Delivery App</h3>
+              <h5>Delivery Platform</h5>
+              <p>This app designed from scratch using UX Research techniques such as interviews, surveys, from paper sketches to a digital prototype.</p>
+              <a href="#" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">Nuxt.js Websites</h3>
+              <h5>Small Businesses & Companies</h5>
+              <p>It’s has been my choice for building websites for small scales businesses and corporate websites. Here are some of the nuxt.JS projects that i recently built.</p>
+              <a href="https://gtrendz.online" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+        </div>
+        <h4 class="FontRegular mt-4">Open Source Contributions</h4>
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="OpenSource_item">
+              <h4>DevTools</h4>
+              <p>It’s a small project, a website for developer tools. I contributed to redesigning the site with the latest bootstrap framework a while ago.</p>
+              <a href="#" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="OpenSource_item">
+              <h4>Felix App</h4>
+              <p>I have been contributing to design and frontend development (UX/UI) for this dockerized file manager on GitHub</p>
+              <a href="#" rel="no-follow" class="ProjectLink">website</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+    <hr class="container" />
+    <section>
+      <div class="container">
+        <h3>Case Study</h3>
+        <h4 class="FontRegular">Explore the case studies of my projects</h4>
+        
+      </div>
+    </section>
+    <hr class="container" />
   </div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-
 export default {
-  components: {
-    Logo
-  }
+  
 };
 </script>
