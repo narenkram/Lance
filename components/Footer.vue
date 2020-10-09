d43434b3b4d3df9258e28b70e58a11d38897bef9
Narendra Ram <nkr@aroliant.com>
2020-10-09 19:41:54 +0530

    feat(ui): add responsive spacing

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-  <footer class="bg-gray5 py-3 mt-6">
+  <footer class="bg-gray5 py-3 mt-4 mt-md-5 mt-lg-6">
     <div class="container">
       <div class="row">
-        <div class="col-lg-6">
+        <div class="col-12 col-md-7 col-lg-7">
           <div class="row">
             <div class="col-auto">
               <img src="~/assets/images/logo.svg" class="Logo" alt="narendraram designer" />
@@ -15,7 +15,7 @@
             </div>
           </div>
         </div>
-        <div class="col-lg-6 text-right">
+        <div class="col-12 col-md-5 col-lg-5 text-center text-md-right text-lg-right mt-2 mt-md-0 mt-lg-0">
           <p class="m-0 Footer_paragraph">
             Designed using Figma <br />Hosted on <a href="#" class="Link_underline_footer">GitHub Pages</a>
           </p>
