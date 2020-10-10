9b29ac92b6de8e1ecfb95c45b08ecd86dd9555de
Narendra Ram <nkr@aroliant.com>
2020-10-10 12:47:51 +0530

    fix(ui): mobile header

--- components/Header.vue
+++ components/Header.vue
@@ -18,19 +18,34 @@
       >
         <img src="~/assets/icons/menu.svg" alt="" />
       </button>
-      <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <div
+        class="collapse navbar-collapse"
+        id="collapsibleNavId"
+      >
         <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="#about">About</a>
+            <a class="p-1 p-md-0 p-lg-0 nav-link ml-0" href="#about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link ml-2" href="#services">Services</a>
+            <a
+              class="p-1 p-md-0 p-lg-0 nav-link ml-0 ml-md-2 ml-lg-2"
+              href="#services"
+              >Services</a
+            >
           </li>
           <li class="nav-item">
-            <nuxt-link class="nav-link ml-2" to="/pricing">Pricing</nuxt-link>
+            <nuxt-link
+              class="p-1 p-md-0 p-lg-0 nav-link ml-0 ml-md-2 ml-lg-2"
+              to="/pricing"
+              >Pricing</nuxt-link
+            >
           </li>
           <li class="nav-item">
-            <a class="nav-link ml-2" href="#contact">Contact</a>
+            <a
+              class="p-1 p-md-0 p-lg-0 nav-link ml-0 ml-md-2 ml-lg-2"
+              href="#contact"
+              >Contact</a
+            >
           </li>
         </ul>
       </div>
