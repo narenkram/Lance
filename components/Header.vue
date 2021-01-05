7f7ac6640b4a0a3e60b961cf53edef8a936d99ff
Narendra Ram <narenramm@gmail.com>
2021-01-05 19:32:34 +0530

    feat(blog): add link to blog

--- components/Header.vue
+++ components/Header.vue
@@ -18,10 +18,7 @@
       >
         <img src="~/assets/icons/menu.svg" alt="" />
       </button>
-      <div
-        class="collapse navbar-collapse"
-        id="collapsibleNavId"
-      >
+      <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
           <li class="nav-item">
             <a class="p-1 p-md-0 p-lg-0 nav-link ml-0" href="#about">About</a>
@@ -45,6 +42,13 @@
               class="p-1 p-md-0 p-lg-0 nav-link ml-0 ml-md-2 ml-lg-2"
               href="#contact"
               >Contact</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="p-1 p-md-0 p-lg-0 nav-link ml-0 ml-md-2 ml-lg-2"
+              href="https://blog.narendra.design"
+              >Blog</a
             >
           </li>
         </ul>
