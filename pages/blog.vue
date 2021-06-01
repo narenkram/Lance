8d1d29f71abd83bebc4ad42eada664bdede2cf49
Narendra Ram <narenramm@gmail.com>
2021-06-01 21:27:24 +0530

    a lot minor style improvements
    mostly typography(font size), add new image, shadow improvements, remove bootstrap's default navbar style - to fix nav usability.

--- pages/blog.vue
+++ pages/blog.vue
@@ -7,7 +7,7 @@
             You are now leaving <br />
             <span class="text-secondary">ram.narendra.design</span>
           </h4>
-          <p>
+          <p class="Text_M Text_dark">
             Click
             <a href="https://narendra.design">here</a> to visit
             <a href="https://narendra.design"
