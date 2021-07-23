0fb5757cc306a6922b9be5b0f6dca41b081fdc6a
Narendra Ram <narenramm@gmail.com>
2021-07-23 15:51:31 +0530

    thank God the code works without flaw
    complete the language toggler with the help of window onload

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,12 +4,17 @@ window.dataLayer = window.dataLayer || [];
 
   gtag('config', 'G-848NRXZRM8');
 
-
+window.onload = (event) => {
+    console.log('page is fully loaded');
+    myFunction();
+    console.log('function called to initialize variable tamil');
+};
 
 function myFunction() {
-    console.log("function call success")
-    var eng_elements = document.getElementsByClassName("english")
-    var tamil_elements = document.getElementsByClassName("tamil")
+    console.log("function call success");
+    var eng_elements = document.getElementsByClassName("english");
+    var tamil_elements = document.getElementsByClassName("tamil");
+    console.log("tamil initialized");
 
     for (var i = 0; i < eng_elements.length && i < tamil_elements.length; i++){
         console.log("for loop starts");
@@ -17,12 +22,12 @@ function myFunction() {
         if(tamil_elements[i].style.display === "none"){
             eng_elements[i].style.display = "none";
             tamil_elements[i].style.display = "block";
-            console.log("english is hidden");
+            console.log("english elements are now hidden");
         }
         else{
             eng_elements[i].style.display = "block";
             tamil_elements[i].style.display = "none";
-            console.log("tamil is hidden");
+            console.log("tamil elements are now hidden");
         }
         console.log("the end");
     }
