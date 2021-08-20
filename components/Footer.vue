325d599d7f8cc057d740404a112623329ac4e35c
Narendra Ram <contact@narendra.design>
2021-08-20 20:06:03 +0530

    remove tamil content

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,82 +1,40 @@
 <template>
   <footer class="Footer py-3 mt-4 mt-md-5 mt-lg-6">
-    <div class="english">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-md-7 col-lg-7">
-            <div class="row text-center text-md-start text-lg-start">
-              <div class="col-12 col-md-auto">
-                <img
-                  src="~/assets/images/logo.svg"
-                  class="Logo mb-1 mb-md-0 mb-lg-0"
-                  alt="narendraram designer"
-                />
-              </div>
-              <div class="col-12 col-md-auto align-self-center">
-                <p class="m-0 Footer_paragraph">
-                  Narendra Ram <br />
-                  Freelance Full Stack Designer
-                </p>
-              </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-7 col-lg-7">
+          <div class="row text-center text-md-start text-lg-start">
+            <div class="col-12 col-md-auto">
+              <img
+                src="~/assets/images/logo.svg"
+                class="Logo mb-1 mb-md-0 mb-lg-0"
+                alt="narendraram designer"
+              />
             </div>
-          </div>
-          <div
-            class="
-              col-12 col-md-5 col-lg-5
-              text-center text-md-end text-lg-end
-              mt-2 mt-md-0 mt-lg-0
-            "
-          >
-            <p class="m-0 Footer_paragraph">
-              Designed using Figma <br />Hosted on
-              <a
-                href="https://github.com/narenkram/narenkram"
-                target="_blank"
-                class="Link_underline_footer"
-                >GitHub Pages</a
-              >
-            </p>
+            <div class="col-12 col-md-auto align-self-center">
+              <p class="m-0 Footer_paragraph">
+                Narendra Ram <br />
+                Freelance Full Stack Designer
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="tamil">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-md-7 col-lg-7">
-            <div class="row text-center text-md-start text-lg-start">
-              <div class="col-12 col-md-auto">
-                <img
-                  src="~/assets/images/logo.svg"
-                  class="Logo mb-1 mb-md-0 mb-lg-0"
-                  alt="narendraram designer"
-                />
-              </div>
-              <div class="col-12 col-md-auto align-self-center">
-                <p class="m-0 Footer_paragraph">
-                  நரேந்திர ராம்<br />
-                  சுயதொழில் பயனர் இடைமுகம் வடிவமைப்பாளர்
-                </p>
-              </div>
-            </div>
-          </div>
-          <div
-            class="
-              col-12 col-md-5 col-lg-5
-              text-center text-md-end text-lg-end
-              mt-2 mt-md-0 mt-lg-0
-            "
-          >
-            <p class="m-0 Footer_paragraph">
-              ஃபிக்மாவைப் பயன்படுத்தி வடிவமைக்கப்பட்டுள்ளது<br />இந்த வலைத்தளம்
-              <a
-                href="https://github.com/narenkram/narenkram"
-                target="_blank"
-                class="Link_underline_footer"
-                >கித்துப் பக்கத்தில்</a
-              >வாழ்கிறது
-            </p>
-          </div>
+        <div
+          class="
+            col-12 col-md-5 col-lg-5
+            text-center text-md-end text-lg-end
+            mt-2 mt-md-0 mt-lg-0
+          "
+        >
+          <p class="m-0 Footer_paragraph">
+            Designed using Figma <br />Hosted on
+            <a
+              href="https://github.com/narenkram/narenkram"
+              target="_blank"
+              class="Link_underline_footer"
+              >GitHub Pages</a
+            >
+          </p>
         </div>
       </div>
     </div>
